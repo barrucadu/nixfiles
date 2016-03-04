e5b891e7d7b25dca6521c1a5b904adc3c9d483dc
Michael Walker <mike@barrucadu.co.uk>
2016-03-04 15:44:36 +0000

    Add pulseaudio and nvidia

--- example-configuration.nix
+++ example-configuration.nix
@@ -18,22 +18,14 @@
       #./services/xserver.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
+  # Use GRUB 2.
+  boot.loader.grub.enable  = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device  = "/dev/sda";
 
-  networking.hostName = "yig"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # Enable nvidia graphics
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  #hardware.opengl.driSupport32Bit = true;
 
   # Holgate wifi issue
   #boot.kernelPackages = pkgs.linuxPackages_4_4;
@@ -44,4 +36,12 @@
   #    ];
   #  };
   #};
+
+  # Enable pulseaudio
+  #hardware.pulseaudio.enable = true;
+  #hardware.pulseaudio.support32Bit = true;
+
+  # Enable wifi
+  networking.hostName = "myHostname";
+  #networking.wireless.enable = true;
 }
