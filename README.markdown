4d85431c47d64c9cc47e9b2ae11f0c9adfd552fc
Michael Walker <mike@barrucadu.co.uk>
2016-03-05 14:37:04 +0000

    Add concrete configuration, drop example

--- README.markdown
+++ README.markdown
@@ -2,6 +2,6 @@ nixfiles
 ========
 
 My [NixOS][] configuration and assorted other crap. Clone to `/etc/nixos` and
-produce a new configuration.nix file based off the example one.
+produce a new configuration.nix file based off the example ones.
 
 [NixOS]: https://nixos.org
---
+++ azathoth.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+let
+  nfsShare = name:
+    { device = "nyarlathotep:/${name}"
+    ; fsType = "nfs"
+    ; options = [ "x-systemd.automount" "noauto" ]
+    ; };
+in
+
+{
+  networking.hostName = "azathoth";
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # Include the standard configuration.
+      ./base/default.nix
+
+      # Include other configuration.
+      ./services/mpd.nix
+      #./services/nginx.nix
+      ./services/openssh.nix
+      ./services/xserver.nix
+    ];
+
+  # Use GRUB 2.
+  boot.loader.grub.enable  = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device  = "/dev/sda";
+
+  # Windows
+  fileSystems."/mnt/data".device = "/dev/disk/by-label/Data";
+  fileSystems."/mnt/data".fsType = "ntfs";
+
+  boot.loader.grub.extraEntries = ''
+    menuentry "Windows 10" {
+      set root=(hd1,1)
+      chainloader +1
+    }
+  '';
+
+  # Enable nvidia graphics
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.driSupport32Bit = true;
+
+  # Holgate wifi issue
+  boot.kernelPackages = pkgs.linuxPackages_4_4;
+  nixpkgs.config.packageOverrides = pkgs: {
+    linux_4_4 = pkgs.linux_4_4.override {
+      kernelPatches = pkgs.linux_4_4.kernelPatches ++ [
+        { patch = patches/wifi.patch; name = "wifi.patch"; }
+      ];
+    };
+  };
+
+  # Enable pulseaudio
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  # Enable wifi
+  networking.wireless.enable = true;
+
+  # Nyarlathotep
+  networking.interfaces.enp6s0.ipAddress = "10.1.1.2";
+  networking.interfaces.enp6s0.prefixLength = 24;
+
+  networking.extraHosts = "10.1.1.1 nyarlathotep";
+
+  fileSystems."/home/barrucadu/nfs/anime"    = nfsShare "anime";
+  fileSystems."/home/barrucadu/nfs/music"    = nfsShare "music";
+  fileSystems."/home/barrucadu/nfs/movies"   = nfsShare "movies";
+  fileSystems."/home/barrucadu/nfs/tv"       = nfsShare "tv";
+  fileSystems."/home/barrucadu/nfs/images"   = nfsShare "images";
+  fileSystems."/home/barrucadu/nfs/torrents" = nfsShare "torrents";
+
+  # Extra packages
+  environment.systemPackages = with pkgs; [
+    abcde
+  ];
+}
