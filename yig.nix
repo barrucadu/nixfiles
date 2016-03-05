f518aa6f633cf0860f92b82d4e4a5468f7954ec4
Michael Walker <mike@barrucadu.co.uk>
2016-03-05 15:49:24 +0000

    [yig] Add yig configuration

---
+++ yig.nix
@@ -0,0 +1,40 @@
+{ ... }:
+
+{
+  networking.hostName = "yig";
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+
+      # Include the standard configuration.
+      ./base/default.nix
+
+      # Include other configuration.
+      ./services/openssh.nix
+      ./services/xserver.nix
+    ];
+
+  # Use GRUB 2.
+  boot.loader.grub.enable  = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device  = "/dev/sda";
+
+  # Enable pulseaudio
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  # Enable wifi
+  networking.wireless.enable = true;
+
+  # Enable redshift
+  services.redshift = {
+    enable = true;
+    # York
+    latitude  = "53.953";
+    longitude = "-1.0391";
+  };
+
+  # Enable touchpad
+  services.xserver.synaptics.enable = true;
+}
