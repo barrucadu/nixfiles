df5d2e77e71f3a00fdcaf03d327c8cda02102606
Michael Walker <mike@barrucadu.co.uk>
2017-03-13 01:51:57 +0000

    Remove system packages which I have user environments for.
    - https://github.com/barrucadu/dotfiles/blob/master/misc/bin/nix-persistent-shell
    - https://github.com/barrucadu/dotfiles/tree/master/nix/.nixpkgs/environments

--- base/packages.nix
+++ base/packages.nix
@@ -27,46 +27,29 @@ in
       common = [
         aspell
         aspellDicts.en
-        cabal-install
-        clang
         direnv
         file
-        gcc
-        gdb
-        ghc
         git
-        go_1_6
-        gnumake
         gnupg
         gnupg1compat
         fortune
-        haskellPackages.cpphs
-        haskellPackages.haddock
         haskellPackages.hledger
-        haskellPackages.hlint
-        haskellPackages.hscolour
         haskellPackages.pandoc
         htop
         imagemagick
         lynx
         man-pages
         mosh
-        m4
         nmap
         nox
-        pkgconfig
         psmisc
         python3
-        python2Packages.virtualenv
         python3Packages.pygments
-        python3Packages.virtualenv
         rxvt_unicode.terminfo
-        stack
         stow
         taskwarrior
         tmux
         unzip
-        valgrind
         vim
         which
         whois
