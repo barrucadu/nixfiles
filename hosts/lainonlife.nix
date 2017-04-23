ca47c4ffc52b304d92d546ae32f8ff7a80ae1b2f
Michael Walker <mike@barrucadu.co.uk>
2017-04-24 00:09:20 +0100

    [lainonlife] Initial configuration.

---
+++ hosts/lainonlife.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+
+{
+  networking.hostName = "lainonlife";
+
+  imports = [
+    ./common.nix
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader
+  boot.loader.grub.enable  = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device  = "/dev/sda";
+
+  # OVH network set up
+  networking.interfaces.eno1 = {
+    ip4 = [ { address = "91.121.0.148";           prefixLength = 24;  } ];
+    ip6 = [ { address = "2001:41d0:0001:5394::1"; prefixLength = 128; } ];
+  };
+
+  networking.defaultGateway  = "91.121.0.254";
+  networking.defaultGateway6 = "2001:41d0:0001:53ff:ff:ff:ff:ff";
+
+  networking.nameservers = [ "213.186.33.99" "2001:41d0:3:1c7::1" ];
+
+  # No syncthing
+  services.syncthing.enable = lib.mkForce false;
+}
