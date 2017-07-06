107439ddc40f4e1071011ce9799ec464eebc85fe
Michael Walker <mike@barrucadu.co.uk>
2017-07-06 20:26:14 +0100

    [lainonlife] Bring back the ogg streams

--- hosts/lainonlife/radio.nix
+++ hosts/lainonlife/radio.nix
@@ -17,7 +17,23 @@ let
 
   # Configuration for an MPD instance.
   mpdConfigFor = { channel, description, port, password ? icecastSourcePassword, ... }:
-    pkgs.writeText "mpd-${channel}.conf" ''
+    let shoutConfig = encoder: ext: ''
+      audio_output {
+        name        "[mpd] ${channel} (${ext})"
+        description "${description}"
+        type        "shout"
+        encoder     "${encoder}"
+        host        "localhost"
+        port        "8000"
+        mount       "/mpd-${channel}.${ext}"
+        user        "source"
+        password    "${password}"
+        quality     "3"
+        format      "44100:16:2"
+        always_on   "yes"
+      }
+      '';
+    in pkgs.writeText "mpd-${channel}.conf" ''
       music_directory     "${musicDirFor channel}"
       playlist_directory  "${dataDirFor channel}/playlists"
       db_file             "${dataDirFor channel}/db"
@@ -27,20 +43,8 @@ let
       bind_to_address     "127.0.0.1"
       port                "${toString port}"
 
-      audio_output {
-        name        "${channel} (mp3)"
-        description "${description}"
-        type        "shout"
-        encoder     "lame"
-        host        "localhost"
-        port        "8000"
-        mount       "/mpd-${channel}.mp3"
-        user        "source"
-        password    "${password}"
-        quality     "3"
-        format      "44100:16:2"
-        always_on   "yes"
-      }
+      ${shoutConfig "vorbis" "ogg"}
+      ${shoutConfig "lame"   "mp3"}
 
       audio_output {
         type "null"
@@ -72,24 +76,26 @@ in
     hostname = "lainon.life";
     admin.password = icecastAdminPassword;
     extraConf =
-      let channelMount = { channel, description, password ? icecastSourcePassword, ... }: ''
+      let channelMount = { channel, description, password ? icecastSourcePassword, ... }:
+        let channelMountForExt = ext: ''
             <mount>
-              <mount-name>/${channel}.mp3</mount-name>
+              <mount-name>/${channel}.${ext}</mount-name>
               <password>${password}</password>
-              <fallback-mount>/mpd-${channel}.mp3</fallback-mount>
+              <fallback-mount>/mpd-${channel}.${ext}</fallback-mount>
               <fallback-override>1</fallback-override>
-              <stream-name>${channel}</stream-name>
+              <stream-name>${channel} (${ext}</stream-name>
               <stream-description>${description}</stream-description>
               <public>1</public>
             </mount>
             <mount>
-              <mount-name>/mpd-${channel}.mp3</mount-name>
+              <mount-name>/mpd-${channel}.${ext}</mount-name>
               <password>${password}</password>
-              <stream-name>${channel} (mpd)</stream-name>
+              <stream-name>[mpd] ${channel} (${ext})</stream-name>
               <stream-description>${description}</stream-description>
               <public>0</public>
             </mount>
           '';
+        in channelMountForExt "mp3" + channelMountForExt "ogg";
       in concatMapStringsSep "\n" channelMount channels;
   };
 
