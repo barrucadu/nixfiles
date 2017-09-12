42d425fdce25c5c6b7943a2ae758535de0cb5f45
Michael Walker <mike@barrucadu.co.uk>
2017-09-12 17:28:22 +0100

    [lainonlife] Set a bunch of nginx security headers

--- hosts/lainonlife.nix
+++ hosts/lainonlife.nix
@@ -76,7 +76,12 @@ in
     extraConfig = ''
       header_filter_by_lua_block {
         if not ngx.header["Access-Control-Allow-Origin"] then ngx.header["Access-Control-Allow-Origin"] = "*" end
+        if not ngx.header["Content-Security-Policy"] then ngx.header["Content-Security-Policy"] = "default-src 'self'" end
         if not ngx.header["Referrer-Policy"] then ngx.header["Referrer-Policy"] = "strict-origin-when-cross-origin" end
+        if not ngx.header["Strict-Transport-Security"] then ngx.header["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains" end
+        if not ngx.header["X-Content-Type-Options"] then ngx.header["X-Content-Type-Options"] = "nosniff" end
+        if not ngx.header["X-Frame-Options"] then ngx.header["X-Frame-Options"] = "SAMEORIGIN" end
+        if not ngx.header["X-XSS-Protection"] then ngx.header["X-XSS-Protection"] = "1; mode=block" end
       }
       proxy_max_temp_file_size 0;
     '';
