52daed5f0c1ef92518b1b7c9c1b7bb245cef95ff
Michael Walker <mike@barrucadu.co.uk>
2019-01-14 00:04:07 +0000

    Periodically restart bookdb to flush changes to disk
    I update bookdb at most once every couple of days, so the chance of
    doing an update as the process restarts seems pretty slim.

--- services/bookdb.nix
+++ services/bookdb.nix
@@ -9,6 +9,10 @@
       { ExecStart = "/srv/bookdb/bookdb run bookdb.conf"
       ; Restart   = "on-failure"
       ; WorkingDirectory = "/srv/bookdb"
+      # nasty hack: selda-sqlite doesn't seem to flush to disk without
+      # the connection being closed, so just periodically restart the
+      # service.
+      ; RuntimeMaxSec = "45m"
       ; }
     ; };
 }
