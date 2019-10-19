cad9d19836c280f1b8687badf8ff7f6aca5bd2fa
Michael Walker <mike@barrucadu.co.uk>
2019-10-19 20:16:27 +0100

    Use new phpfpm config format

--- services/nginx-phpfpm.nix
+++ services/nginx-phpfpm.nix
@@ -1,16 +1,18 @@
 { config, ... }:
 
 {
-  services.phpfpm.poolConfigs.nginx = ''
-    user = nginx
-    group = nginx
-    listen = /run/phpfpm/phpfpm.sock
-    listen.owner = nginx
-    listen.group = nginx
-    pm = dynamic
-    pm.max_children = 5
-    pm.start_servers = 2
-    pm.min_spare_servers = 1
-    pm.max_spare_servers = 3
-  '';
+  services.phpfpm.pools.nginx = {
+    user = "nginx";
+    group = "nginx";
+    settings = {
+      "listen" = "/run/phpfpm/phpfpm.sock";
+      "listen.owner" = "nginx";
+      "listen.group" = "nginx";
+      "pm" = "dynamic";
+      "pm.max_children" = "5";
+      "pm.start_servers" = "2";
+      "pm.min_spare_servers" = "1";
+      "pm.max_spare_servers" = "3";
+    };
+  };
 }
