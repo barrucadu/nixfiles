def16fbd40b01c695b4b4becdb91d476147b8316
Michael Walker <mike@barrucadu.co.uk>
2020-02-16 18:17:41 +0000

    [dreamlands] Add gitea service

--- hosts/dreamlands/configuration.nix
+++ hosts/dreamlands/configuration.nix
@@ -2,6 +2,10 @@
 
 with lib;
 
+let
+  giteaHttpPort = 3000;
+in
+
 {
   networking.hostName = "dreamlands";
 
@@ -17,4 +21,40 @@ with lib;
     ipv6.addresses = [ { address = "2a01:4f8:c0c:77b3::"; prefixLength = 64; } ];
   };
   networking.defaultGateway6 = { address = "fe80::1"; interface = "ens3"; };
+
+  # WWW
+  services.caddy.enable = true;
+  services.caddy.enable-phpfpm-pool = true;
+  services.caddy.config = ''
+    (basics) {
+      log / stdout "{host} {combined}"
+      gzip
+    }
+
+    git.barrucadu.dev {
+      import basics
+
+      proxy / http://127.0.0.1:${toString giteaHttpPort} {
+        transparent
+      }
+    }
+  '';
+
+  # Gitea
+  systemd.services.gitea =
+    let
+      dockerComposeYaml = import ./gitea.docker-compose.nix { httpPort = giteaHttpPort; };
+      dockerComposeFile = pkgs.writeText "docker-compose.yml" dockerComposeYaml;
+    in
+      {
+        enable   = true;
+        wantedBy = [ "multi-user.target" ];
+        requires = [ "docker.service" ];
+        environment = { COMPOSE_PROJECT_NAME = "gitea"; };
+        serviceConfig = {
+          ExecStart = "${pkgs.docker_compose}/bin/docker-compose -f '${dockerComposeFile}' up";
+          ExecStop  = "${pkgs.docker_compose}/bin/docker-compose -f '${dockerComposeFile}' stop";
+          Restart   = "always";
+        };
+      };
 }
---
+++ hosts/dreamlands/gitea.docker-compose.nix
@@ -0,0 +1,60 @@
+{ httpPort            ? 3000
+, sshPort             ? 222
+, internalHTTP        ? true
+, internalSSH         ? false
+, giteaTag            ? "1.11"
+, postgresTag         ? "9.6"
+}:
+
+''
+version: "2"
+
+services:
+  server:
+    image: gitea/gitea:${giteaTag}
+    environment:
+      - APP_NAME="barrucadu.dev git"
+      - RUN_MODE=prod
+      - ROOT_URL=https://git.barrucadu.dev
+      - SSH_DOMAIN=barrucadu.dev
+      - SSH_PORT=${toString sshPort}
+      - SSH_LISTEN_PORT=22
+      - HTTP_PORT=3000
+      - DB_TYPE=postgres
+      - DB_HOST=db:5432
+      - DB_NAME=gitea
+      - DB_USER=gitea
+      - DB_PASSWD=gitea
+      - USER_UID=1000
+      - USER_GID=1000
+    restart: always
+    networks:
+      - gitea
+    volumes:
+      - gitea_data:/data
+    ports:
+      - "${if internalHTTP then "127.0.0.1:" else ""}${toString httpPort}:3000"
+      - "${if internalSSH  then "127.0.0.1:" else ""}${toString sshPort}:22"
+    depends_on:
+      - db
+
+  db:
+    image: postgres:${postgresTag}
+    restart: always
+    environment:
+      - POSTGRES_USER=gitea
+      - POSTGRES_PASSWORD=gitea
+      - POSTGRES_DB=gitea
+    networks:
+      - gitea
+    volumes:
+      - gitea_postgres:/var/lib/postgresql/data
+
+networks:
+  gitea:
+    external: false
+
+volumes:
+  gitea_data:
+  gitea_postgres:
+''
