d79116c81a363ba29873cbc1400cd1b5f94717a6
Michael Walker <mike@barrucadu.co.uk>
2020-02-20 14:30:34 +0000

    [dreamlands] Open port 222
    For gitea ssh

--- hosts/dreamlands/configuration.nix
+++ hosts/dreamlands/configuration.nix
@@ -47,7 +47,7 @@ in
   boot.loader.grub.device  = "/dev/sda";
 
   # Networking
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 80 222 443 ];
 
   networking.interfaces.ens3 = {
     ipv6.addresses = [ { address = "2a01:4f8:c0c:77b3::"; prefixLength = 64; } ];
