52cfadb01b84b239f70fa07787b57863617bc177
Michael Walker <mike@barrucadu.co.uk>
2020-11-03 17:24:33 +0000

    Bump finder to elasticsearch-7.9.3

--- services/finder.nix
+++ services/finder.nix
@@ -61,7 +61,7 @@ in
     image = mkOption { type = types.str; };
     httpPort = mkOption { type = types.int; default = 3000; };
     internalHTTP = mkOption { type = types.bool; default = true; };
-    esTag = mkOption { type = types.str; default = "7.6.2"; };
+    esTag = mkOption { type = types.str; default = "7.9.3"; };
     execStartPre = mkOption { type = types.str; default = ""; };
     dockerVolumeDir = mkOption { type = types.path; default = /no-path; };
     mangaDir = mkOption { type = types.path; };
