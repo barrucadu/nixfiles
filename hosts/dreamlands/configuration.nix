8485225baeb9ef2a9afd8e9d39a441d5e93abed1
Michael Walker <mike@barrucadu.co.uk>
2021-03-12 21:24:23 +0000

    [dreamlands] Remove frontend and event-api-server
    Originally I planned to do more with the event-api-server; but in
    practice all it's been used for is for Concourse job status.
    Gathering that information adds significant complication to all the
    pipelines, so I've decided to remove it.

--- hosts/dreamlands/configuration.nix
+++ hosts/dreamlands/configuration.nix
@@ -4,8 +4,6 @@ with lib;
 
 let
   concourseHttpPort = 3001;
-  eventApiHttpPort  = 3002;
-  frontendHttpPort  = 3003;
   giteaHttpPort     = 3000;
   registryHttpPort  = 5000;
 
@@ -57,19 +55,6 @@ in
   # WWW
   services.caddy.enable = true;
   services.caddy.config = ''
-    barrucadu.dev {
-      redir https://www.barrucadu.dev{uri}
-    }
-
-    dreamlands.barrucadu.co.uk {
-      redir https://www.barrucadu.dev{uri}
-    }
-
-    www.barrucadu.dev {
-      encode gzip
-      reverse_proxy http://127.0.0.1:${toString frontendHttpPort}
-    }
-
     registry.barrucadu.dev {
       encode gzip
       basicauth /v2/* {
@@ -77,11 +62,6 @@ in
       }
       header /v2/* Docker-Distribution-Api-Version "registry/2.0"
       reverse_proxy /v2/* http://127.0.0.1:${toString registryHttpPort}
-    }
-
-    event-api.barrucadu.dev {
-      encode gzip
-      reverse_proxy http://127.0.0.1:${toString eventApiHttpPort}
     }
 
     cd.barrucadu.dev {
@@ -112,41 +92,9 @@ in
       githubClientSecret = fileContents /etc/nixos/secrets/concourse-clientsecret.txt;
     };
   };
-  security.sudo.extraRules = [
-    {
-      users = [ "concourse-deploy-robot" ];
-      commands = [
-        { command = "${pkgs.systemd}/bin/systemctl restart event-api-server"; options = [ "NOPASSWD" ]; }
-        { command = "${pkgs.systemd}/bin/systemctl restart frontend";         options = [ "NOPASSWD" ]; }
-      ];
-    }
-  ];
-  users.extraUsers.concourse-deploy-robot = {
-    home = "/home/system/concourse-deploy-robot";
-    createHome = true;
-    isSystemUser = true;
-    openssh.authorizedKeys.keys =
-      [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxmx52eoKFJmdkejuiLZ4ZMaQ/4GQsXADIORQdmmb8N concourse-worker@cd.barrucadu.dev" ];
-    shell = pkgs.bashInteractive;
-  };
 
   systemd.services.gitea = dockerComposeService {
     name = "gitea";
     yaml = import ./gitea.docker-compose.nix { httpPort = giteaHttpPort; };
-  };
-
-  systemd.services.frontend = dockerComposeService {
-    name = "frontend";
-    yaml = import ./frontend.docker-compose.nix { httpPort = frontendHttpPort; };
-    pull = "frontend:latest";
-  };
-
-  systemd.services.event-api-server = dockerComposeService {
-    name = "event-api-server";
-    yaml = import ./event-api-server.docker-compose.nix {
-      httpPort  = eventApiHttpPort;
-      jwtSecret = fileContents /etc/nixos/secrets/event-api-server-jwt.txt;
-    };
-    pull = "event-api-server:latest";
   };
 }
