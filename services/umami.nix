1db93f30a77c75598dc5f04aa9f60bcb27082b62
Michael Walker <mike@barrucadu.co.uk>
2021-09-03 16:52:24 +0100

    Migrate `umami` to `virtualisation.oci-containers`

--- services/umami.nix
+++ services/umami.nix
@@ -1,10 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 with lib;
 let
   cfg = config.services.umami;
+  backend = config.virtualisation.oci-containers.backend;
 
-  yaml = import ./docker-compose-files/umami.docker-compose.nix cfg;
+  # https://github.com/NixOS/nixpkgs/issues/104750
+  serviceConfigForContainerLogging = { StandardOutput = mkForce "journal"; StandardError = mkForce "journal"; };
 in
 {
   options.services.umami = {
@@ -18,10 +20,36 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.services.umami = import ./snippets/docker-compose-service.nix {
-      inherit lib pkgs yaml;
-      composeProjectName = "umami";
-      execStartPre = cfg.execStartPre;
+    virtualisation.oci-containers.containers.umami = {
+      autoStart = true;
+      image = "ghcr.io/mikecao/umami:${cfg.umamiTag}";
+      environment = {
+        "DATABASE_URL" = "postgres://umami:umami@umami-db/umami";
+        "HASH_SALT" = cfg.hashSalt;
+      };
+      extraOptions = [ "--network=umami_network" ];
+      dependsOn = [ "umami-db" ];
+      ports = [ "127.0.0.1:${toString cfg.httpPort}:3000" ];
+    };
+    systemd.services."${backend}-umami" = {
+      preStart = mkIf (cfg.execStartPre != null) cfg.execStartPre;
+      serviceConfig = serviceConfigForContainerLogging;
+    };
+
+    virtualisation.oci-containers.containers.umami-db = {
+      autoStart = true;
+      image = "postgres:${cfg.postgresTag}";
+      environment = {
+        "POSTGRES_DB" = "umami";
+        "POSTGRES_USER" = "umami";
+        "POSTGRES_PASSWORD" = "umami";
+      };
+      extraOptions = [ "--network=umami_network" ];
+      volumes = [ "${toString cfg.dockerVolumeDir}/pgdata:/var/lib/postgresql/data" ];
+    };
+    systemd.services."${backend}-umami-db" = {
+      preStart = "${backend} network create -d bridge umami_network || true";
+      serviceConfig = serviceConfigForContainerLogging;
     };
   };
 }
