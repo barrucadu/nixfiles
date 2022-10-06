b85ef35161f3be06503b4e8368af4bbf027aed73
Michael Walker <mike@barrucadu.co.uk>
2022-10-06 16:09:24 +0100

    Remove repetition of system and pkgs

--- flake.nix
+++ flake.nix
@@ -8,92 +8,93 @@
     };
   };
 
-  outputs = { nixpkgs, sops-nix, ... }@flakeInputs: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
-    nixosConfigurations.azathoth = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit flakeInputs; };
+  outputs = { nixpkgs, sops-nix, ... }@flakeInputs:
+    let
       system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./hosts/azathoth/configuration.nix
-        ./hosts/azathoth/hardware.nix
-        "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-      ];
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      nixosConfigurations.azathoth = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit flakeInputs; };
+        modules = [
+          ./common.nix
+          ./hosts/azathoth/configuration.nix
+          ./hosts/azathoth/hardware.nix
+          "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+        ];
+      };
+
+      nixosConfigurations.carcosa = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit flakeInputs; };
+        modules = [
+          ./common.nix
+          ./hosts/carcosa/configuration.nix
+          ./hosts/carcosa/hardware.nix
+          "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+          sops-nix.nixosModules.sops
+        ];
+      };
+
+      nixosConfigurations.lainonlife = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit flakeInputs; };
+        modules = [
+          ./common.nix
+          ./hosts/lainonlife/configuration.nix
+          ./hosts/lainonlife/hardware.nix
+          "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+          sops-nix.nixosModules.sops
+        ];
+      };
+
+      nixosConfigurations.nyarlathotep = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit flakeInputs; };
+        modules = [
+          ./common.nix
+          ./hosts/nyarlathotep/configuration.nix
+          ./hosts/nyarlathotep/hardware.nix
+          "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+          sops-nix.nixosModules.sops
+        ];
+      };
+
+      packages.${system} = {
+        backups =
+          let
+            pythonEnv = pkgs.python3.buildEnv.override { extraLibs = with pkgs.python3Packages; [ boto3 ]; };
+          in
+          pkgs.writeShellScriptBin "backups.sh" ''
+            #!${pkgs.bash}
+
+            PATH=${pkgs.duplicity}/bin:${pkgs.python3}/bin:${pkgs.sops}/bin:${pkgs.nettools}/bin
+            PYTHONPATH="${pythonEnv}/${pkgs.python3.sitePackages}/"
+
+            ${pkgs.lib.fileContents ./scripts/backups.sh}
+          '';
+
+        lint =
+          pkgs.writeShellScriptBin "lint.sh" ''
+            #!${pkgs.bash}
+
+            PATH=${pkgs.nix-linter}/bin:${pkgs.shellcheck}/bin
+
+            ${pkgs.lib.fileContents ./scripts/lint.sh}
+          '';
+
+        secrets =
+          pkgs.writeShellScriptBin "backups.sh" ''
+            #!${pkgs.bash}
+
+            PATH=${pkgs.sops}/bin:${pkgs.nettools}/bin
+            EDITOR=${pkgs.vim}/bin/vim
+
+            ${pkgs.lib.fileContents ./scripts/secrets.sh}
+          '';
+      };
     };
-
-    nixosConfigurations.carcosa = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit flakeInputs; };
-      system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./hosts/carcosa/configuration.nix
-        ./hosts/carcosa/hardware.nix
-        "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
-        sops-nix.nixosModules.sops
-      ];
-    };
-
-    nixosConfigurations.lainonlife = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit flakeInputs; };
-      system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./hosts/lainonlife/configuration.nix
-        ./hosts/lainonlife/hardware.nix
-        "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-        sops-nix.nixosModules.sops
-      ];
-    };
-
-    nixosConfigurations.nyarlathotep = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit flakeInputs; };
-      system = "x86_64-linux";
-      modules = [
-        ./common.nix
-        ./hosts/nyarlathotep/configuration.nix
-        ./hosts/nyarlathotep/hardware.nix
-        "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-        sops-nix.nixosModules.sops
-      ];
-    };
-
-
-    packages.x86_64-linux.backups =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        pythonEnv = pkgs.python3.buildEnv.override { extraLibs = with pkgs.python3Packages; [ boto3 ]; };
-      in
-      pkgs.writeShellScriptBin "backups.sh" ''
-        #!${pkgs.bash}
-
-        PATH=${pkgs.duplicity}/bin:${pkgs.python3}/bin:${pkgs.sops}/bin:${pkgs.nettools}/bin
-        PYTHONPATH="${pythonEnv}/${pkgs.python3.sitePackages}/"
-
-        ${pkgs.lib.fileContents ./scripts/backups.sh}
-      '';
-
-    packages.x86_64-linux.lint =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.writeShellScriptBin "lint.sh" ''
-        #!${pkgs.bash}
-
-        PATH=${pkgs.nix-linter}/bin:${pkgs.shellcheck}/bin
-
-        ${pkgs.lib.fileContents ./scripts/lint.sh}
-      '';
-
-    packages.x86_64-linux.secrets =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.writeShellScriptBin "backups.sh" ''
-        #!${pkgs.bash}
-
-        PATH=${pkgs.sops}/bin:${pkgs.nettools}/bin
-        EDITOR=${pkgs.vim}/bin/vim
-
-        ${pkgs.lib.fileContents ./scripts/secrets.sh}
-      '';
-  };
 }
