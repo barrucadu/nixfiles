6083056537865f426337ff917ababb05ce9429de
Michael Walker <mike@barrucadu.co.uk>
2023-03-16 18:37:25 +0000

    Run bookmarks outside of docker
    Natively packaging this application isn't very hard.  It does make the
    sync script a little more complex, as it's got to work out the
    appropriate path to the python environment, but that's not too tricky.
    
    This also makes deploying updates to nyarlathotep a little easier: I
    don't need to build a docker image, push it to a local repo, and restart
    the service any more.  I just bump the version in the package, same as
    resolved and prometheus-awair-exporter.
    
    Needing to expose the Elasticsearch instance to localhost is a bit
    unfortunate, however.  Maybe I'll be able to do some systemd network
    namespacing magic and lock that down at some point.
    
    I also changed the bookdb port to be a random one in the high end of the
    user ports range which is unlikely to clash with anything else.

--- hosts/nyarlathotep/jobs/bookmarks-sync.sh
+++ hosts/nyarlathotep/jobs/bookmarks-sync.sh
@@ -4,11 +4,13 @@ set -euo pipefail
 
 local_sync_dir="$(mktemp -d)"
 remote_sync_dir="/tmp/bookmarks-sync"
+es_host="$(systemctl cat bookmarks | grep ES_HOST | cut -d'=' -f3 | sed 's/"//')"
+python="$(systemctl cat bookmarks | grep ExecStart | sed 's/^ExecStart=//' | sed 's/gunicorn.*/python/')"
 
 # shellcheck disable=SC2064
 trap "rm -rf $local_sync_dir" EXIT
 
-docker exec -i bookmarks env ES_HOST=http://bookmarks-db:9200 python -m bookmarks.index.dump > "${local_sync_dir}/dump.json"
+env "ES_HOST=$es_host" "$python" -m bookmarks.index.dump > "${local_sync_dir}/dump.json"
 
 scp -r "$local_sync_dir" "carcosa.barrucadu.co.uk:${remote_sync_dir}"
 
@@ -16,8 +18,11 @@ scp -r "$local_sync_dir" "carcosa.barrucadu.co.uk:${remote_sync_dir}"
 ssh carcosa.barrucadu.co.uk <<EOF
 set -euo pipefail
 
+es_host="\$(systemctl cat bookmarks | grep ES_HOST | cut -d'=' -f3 | sed 's/"//')"
+python="\$(systemctl cat bookmarks | grep ExecStart | sed 's/^ExecStart=//' | sed 's/gunicorn.*/python/')"
+
 trap "rm -rf $remote_sync_dir" EXIT
 
 cd "$remote_sync_dir"
-docker exec -i bookmarks env DELETE_EXISTING_INDEX=1 ES_HOST=http://bookmarks-db:9200 python -m bookmarks.index.create - < dump.json
+env DELETE_EXISTING_INDEX=1 ES_HOST=\$es_host \$python -m bookmarks.index.create - < dump.json
 EOF
