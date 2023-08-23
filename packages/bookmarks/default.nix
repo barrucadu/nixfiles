d9a7b31f9b572f471270558b75f7373b3bf4ee4e
Michael Walker <mike@barrucadu.co.uk>
2023-08-23 01:07:03 +0100

    Bump bookmarks

--- packages/bookmarks/default.nix
+++ packages/bookmarks/default.nix
@@ -5,12 +5,26 @@ let
     projectDir = fetchFromGitHub {
       owner = "barrucadu";
       repo = "bookmarks";
-      rev = "c631c387c10cb8ff090ea90515734c3954a10079";
-      sha256 = "sha256-5QxYJ5Tofhfxu9ixZNEOR4BT+Wwx/HdBKvpSZv6WB0c=";
+      rev = "555dddba3d8e05ac29b3cb282ed76c997ff752c6";
+      sha256 = "sha256-VNEhhPLxEsDSmZzO3QMPK3TVSfOpiPntLTVcWNVmUHY=";
     };
 
-    overrides = poetry2nix.overrides.withDefaults (_: super: {
-      elastic-transport = super.elastic-transport.overridePythonAttrs (old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; });
+    overrides = poetry2nix.overrides.withDefaults (self: super: {
+      beautifulsoup4 = super.beautifulsoup4.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.hatchling ];
+      });
+      elastic-transport = super.elastic-transport.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools ];
+      });
+      flask = super.flask.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools self.flit-core ];
+      });
+      gunicorn = super.gunicorn.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools self.packaging ];
+      });
+      werkzeug = super.werkzeug.overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.setuptools self.flit-core ];
+      });
     });
   };
 in
