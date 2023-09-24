8258218200564b2e289a17b4b71433207b9a486a
Michael Walker <mike@barrucadu.co.uk>
2023-09-24 15:30:13 +0100

    Bump resolved
    This brings IPv6 support, not that I use that.

--- packages/resolved/default.nix
+++ packages/resolved/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "resolved";
-  version = "32c92d919a440c53f8a127d4e04ce848284260e5";
+  version = "c065a4afaffcb2599708d3837b0ce4986b2465c7";
 
   src = fetchFromGitHub {
     owner = "barrucadu";
     repo = pname;
     rev = version;
-    sha256 = "sha256-L1ix6/w7G9McBBio2Q9AG9KB/C0Sfc4goZFmYvPDVT0=";
+    sha256 = "sha256-6KCL/FyETdHpncx2HorpIw20pw9sI2aJSd7hJREWmIM=";
   };
 
-  cargoSha256 = "sha256-D/sqG3PnCS0ormV+PgZLk/p6fSOyhcCNaHV3qRXDh5Q=";
+  cargoSha256 = "sha256-69Q31l28yV+FbSrI/EaPmgLgDNJcAOQ0zmZQi43ZqOQ=";
 
   postInstall = ''
     cd config
