78a1cd2f7a8a1b4e0ed0baecc19cc1d0bb4c67a5
Michael Walker <mike@barrucadu.co.uk>
2023-10-16 22:43:23 +0100

    Upgrade umami to postgres 16
    This has been manually applied to carcosa.

--- shared/umami/options.nix
+++ shared/umami/options.nix
@@ -22,7 +22,7 @@ with lib;
 
     postgresTag = mkOption {
       type = types.str;
-      default = "13";
+      default = "16";
       description = mdDoc ''
         Tag to use of the `postgres` container image.
       '';
