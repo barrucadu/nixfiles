218353f37c1fb8b96c3c8955e93640959bc361fd
Michael Walker <mike@barrucadu.co.uk>
2024-12-10 21:52:14 +0000

    [yuggoth] Include barrucadu-website-mirror template

--- hosts/yuggoth/configuration.nix
+++ hosts/yuggoth/configuration.nix
@@ -13,6 +13,14 @@
 
 with lib;
 {
+  imports = [
+    ../_templates/barrucadu-website-mirror.nix
+  ];
+
+  ###############################################################################
+  ## General
+  ###############################################################################
+
   networking.hostId = "62f520b4";
   boot.supportedFilesystems = { zfs = true; };
 
@@ -28,5 +36,16 @@ with lib;
   nixfiles.eraseYourDarlings.machineId = "ee9cfe217f0f4d45bab5e897e782ca91";
   nixfiles.eraseYourDarlings.barrucaduPasswordFile = config.sops.secrets."users/barrucadu".path;
   sops.secrets."users/barrucadu".neededForUsers = true;
-}
 
+  ###############################################################################
+  ## Nyarlathotep Sync
+  ###############################################################################
+
+  nixfiles.bookdb.remoteSync.receive.enable = true;
+  nixfiles.bookdb.remoteSync.receive.authorizedKeys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIChVw9DPLafA3lCLCI4Df9rYuxedFQTXAwDOOHUfZ0Ac remote-sync@nyarlathotep" ];
+
+  nixfiles.bookmarks.remoteSync.receive.enable = true;
+  nixfiles.bookmarks.remoteSync.receive.authorizedKeys =
+    [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIChVw9DPLafA3lCLCI4Df9rYuxedFQTXAwDOOHUfZ0Ac remote-sync@nyarlathotep" ];
+}
